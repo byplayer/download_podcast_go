3b9c24331953e07c9d10af2e942ad8dd4c4074bc
byplayer <byplayer100@gmail.com>
2020-05-11 09:06:37 +0900

    :+1: Add members for rss

--- models/rss.go
+++ models/rss.go
@@ -6,5 +6,18 @@ type Enclosure struct {
 	Type   string `xml:"type,attr"`
 }
 
+type Item struct {
+	Enclosure Enclosure `xml:"enclosure`
+}
+
+type Channel struct {
+	Title string `xml:"title"`
+	Link string `xml:"link"`
+	Language string `xml:language:`
+	Copyright string `xml:"copyright"`
+	Items[] Item `xml:"item"`
+}
+
 type Rss struct {
+	Channel Channel `xml:"channel"`
 }
